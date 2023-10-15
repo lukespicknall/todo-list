cf6f9026db91ce7f221c5b46cd8ad7f4003d7741
Luke Spicknall <lukejspicknall@gmail.com>
2023-10-15 18:41:07 -0400

    work to make project functionality. project form now populates on add button click. Need to make default project for tasks to go to so tasks can be added and displayed dynamically based on project[] instead of static taskLis[].

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,11 +2,13 @@ const newProject = (pTitle, pDueDate, pPriority) => {
   const title = pTitle;
   const due = pDueDate;
   const priority = pPriority;
+  const tasks = [];
 
   return {
     title,
     due,
     priority,
+    tasks,
   };
 };
 
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,10 +1,11 @@
 // import newTask from './to-do';
-// import newProject from './project';
+import newProject from './project';
 
 import newTask from './to-do';
 // import displayTasks from './display';
 
 // CREATE MAIN HTML DIVS
+const projectList = [];
 const taskList = [];
 const content = document.getElementById('content');
 // LOAD HANDLES UI
@@ -24,27 +25,27 @@ const load = () => {
   foot.id = 'foot';
   pageBox.append(main, sideBar, head, foot);
 
-  // CREATE ADDTASK BUTTON
-  const addTask = document.createElement('button');
-  addTask.id = 'add-task';
-  addTask.textContent = '+';
-  sideBar.appendChild(addTask);
+  // CREATE addNew BUTTON
+  const addBox = document.createElement('div');
+  addBox.id = 'add-Box';
+  const addNew = document.createElement('button');
+  addNew.id = 'add-new';
+  addNew.textContent = '+';
+  addBox.appendChild(addNew);
+  sideBar.appendChild(addBox);
 
-  // EIF THAT ESTABLISHES AND AHNDLES MOST OF THE UI
+  // EIF THAT ESTABLISHES AND HANDLES MOST OF THE UI
   const handlers = (() => {
     // ADD OVERLAY TO DOM BEFORE addBtn EVENT SO TRANSITION WORKS ON BLUR/FORM
-    const taskOverlay = document.createElement('div');
-    taskOverlay.id = 'task-overlay';
-    content.appendChild(taskOverlay);
+    const formOverlay = document.createElement('div');
+    formOverlay.id = 'form-overlay';
+    content.appendChild(formOverlay);
 
     // CREATE REMOVE BLUR FUNCTION
     const removeBlur = (a) => {
       a.classList.remove('blurred');
     };
-
-    // CREATE TABLE ELEMENTS AND LOOP TASKLIST[] AND SEND EACH OBJS DATA TO TABLE FIELDS.
-    // ADD EDITBTN FUNCTIONALITY
-    const displayTasks = () => {
+    const displayProjects = () => {
       taskTable.innerHTML = ''; // clears current taskTable to avoid repeats
       const taskHeader = document.createElement('thead');
       taskHeader.id = 'task-header';
@@ -132,6 +133,98 @@ const load = () => {
         taskComplete(completeBtn);
       }
     };
+    // CREATE TABLE ELEMENTS AND LOOP TASKLIST[] AND SEND EACH OBJS DATA TO TABLE FIELDS.
+    // ADD EDITBTN FUNCTIONALITY
+    const displayTasks = (a, b) => {
+      taskTable.innerHTML = ''; // clears current taskTable to avoid repeats
+      const taskHeader = document.createElement('thead');
+      taskHeader.id = 'task-header';
+      taskHeader.textContent = 'Tasks';
+      taskTable.appendChild(taskHeader);
+      // LOOP THROUGH taskList[]
+      for (let i = 0; i < taskList.length; i += 1) {
+        // CREATE NEW TASK ROW FOR OBJ IN taskList[i]
+        const taskRow = document.createElement('tr');
+        taskRow.className = 'task-row';
+        taskRow.setAttribute('data-id', [i]); // row assigned data-id that is its posiition in taskLisy[]
+        taskTable.appendChild(taskRow); // add that tr to taskTable in libTable
+
+        // CREATE NEW TABLE CELLS FOR TASK DATA
+        const taskCell = document.createElement('td');
+        const notesCell = document.createElement('td');
+        const dueCell = document.createElement('td');
+        const priorityCell = document.createElement('td');
+
+        // ASSIGN CELL CLASS NAMES
+        taskCell.className = 'task-cell';
+        notesCell.className = 'notes-cell';
+        dueCell.className = 'due-cell';
+        priorityCell.className = 'priority-cell';
+
+        // ASSIGN CELL DATA FROM TASK DATA
+        taskCell.textContent = taskList[i].task;
+        notesCell.textContent = taskList[i].notes;
+        dueCell.textContent = taskList[i].due;
+        priorityCell.textContent = taskList[i].priority;
+
+        // CREATE CELLS FOR UI BUTTONS ON ROW FOR TASK OBJ UPDATING
+        const editCell = document.createElement('td'); //
+        const deleteCell = document.createElement('td'); //
+        const completeCell = document.createElement('td'); //
+
+        // ASSIGN UI CELL CLASSNAMES
+        editCell.className = 'edit-cell';
+        deleteCell.className = 'delete-cell';
+        completeCell.className = 'complete-cell'; //
+
+        // CREATE UI BUTTONS FOR TASK UPDATES, ASSIGN CLASS, APPEND TO UI CELL
+        const editBtn = document.createElement('button');
+        editBtn.className = 'edit-btn';
+        editBtn.innerHTML = '<p>edit</p>';
+        editCell.appendChild(editBtn);
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete-btn';
+        deleteBtn.innerHTML = '<p>delete</p>';
+        deleteCell.appendChild(deleteBtn);
+        const completeBtn = document.createElement('button');
+        completeBtn.className = 'complete-btn';
+        completeBtn.innerHTML = '<p>complete</p>';
+        completeCell.appendChild(completeBtn);
+
+        // APPEND CELLS TO THE TASK TROW
+        taskRow.appendChild(taskCell);
+        if (b === 'task') {
+          taskRow.appendChild(notesCell);
+        }
+        taskRow.appendChild(dueCell);
+        taskRow.appendChild(priorityCell);
+        taskRow.appendChild(editCell);
+        taskRow.appendChild(deleteCell);
+        taskRow.appendChild(completeCell);
+
+        // ON CLICK, EDITBTN SETS EDITING STATE,
+        // GRABS OBJECT DATA THROUGH DATA-ID REFERENCE ON ITS TASK ROW,
+        // POPULATES A FORM CONTAINING THE OBJ'S DATA BY PASISNG THAT DATA TO DISPLAYFORM()
+        editBtn.addEventListener('click', () => {
+          const editing = true;
+          const currentTask = taskRow.getAttribute('data-id');
+          const editTitle = taskList[currentTask].task;
+          const editNotes = taskList[currentTask].notes;
+          const editDue = taskList[currentTask].due;
+          const editPriority = taskList[currentTask].priority;
+          displayForm(
+            editing,
+            editTitle,
+            editNotes,
+            editDue,
+            editPriority,
+            currentTask,
+          );
+        });
+        taskDelete(deleteBtn);
+        taskComplete(completeBtn);
+      }
+    };
 
     // FUCNCTIONALITY FOR COMPLETE BUTTON
     const taskComplete = (completeBtn) => {
@@ -163,21 +256,193 @@ const load = () => {
     };
 
     // DISPLAY FORM, USING ARGS PASSED FROM OBJ IF FORM LAUNCHED FORM EDIT BUTTON
-    const displayForm = (a, b, c, d, f, g) => {
+    const displayProjectForm = (a, b, c, d, f) => {
       // BLUR BACKGORUND
       pageBox.classList.add('blurred');
 
-      // SET taskOverlay ID AND CREATE taskCard AND taskFrom
-      taskOverlay.id = 'task-overlay-vis';
-      const taskCard = document.createElement('div');
-      taskCard.id = 'task-card';
-      const taskForm = document.createElement('form');
-      taskForm.id = 'task-form';
-      taskForm.setAttribute('action', '');
-      taskForm.setAttribute('method', 'post');
+      // SET formOverlay ID AND CREATE taskCard AND taskFrom
+      formOverlay.id = 'form-overlay-vis';
+      const projectCard = document.createElement('div');
+      projectCard.id = 'form-card';
+      const projectForm = document.createElement('form');
+      projectForm.id = 'form-form';
+      projectForm.setAttribute('action', '');
+      projectForm.setAttribute('method', 'post');
 
       // CREATE TITLE INPUT AND LABEL, SET ATTRIBUTES
       // IF FORM LAUNCHED FORM editBtn, POPULATE W/ ARG VALUE FORM OBJ
+
+      const titleLabel = document.createElement('label');
+      titleLabel.setAttribute('for', 'project-title');
+      titleLabel.textContent = 'Title';
+      const projectTitle = document.createElement('input');
+      if (a === true) {
+        projectTitle.value = b;
+      }
+      projectTitle.setAttribute('type', 'text');
+      projectTitle.setAttribute('id', 'from-title');
+      projectTitle.setAttribute('placeholder', 'project title . . .');
+      projectTitle.setAttribute('name', 'project-title');
+
+      // CREATE DUE INPUT AND LABEL, SET ATTRIBUTES
+      // IF FORM LAUNCHED FORM editBtn, POPULATE W/ ARG VALUE FORM OBJ
+      const dueLabel = document.createElement('label');
+      dueLabel.setAttribute('for', 'project-due');
+      dueLabel.textContent = 'Due date';
+      const projectDue = document.createElement('input');
+      if (a === true) {
+        projectDue.value = c;
+      }
+      projectDue.setAttribute('id', 'form-due');
+      projectDue.setAttribute('type', 'date');
+      projectDue.setAttribute('placeholder', 'project due . . .');
+      projectDue.setAttribute('name', 'project-due');
+
+      // CREATE PRIORITY INPUT AND LABEL, SET ATTRIBUTES
+      // IF FORM LAUNCHED FORM editBtn, POPULATE W/ ARG VALUE FORM OBJ
+      const priorityLabel = document.createElement('label');
+      // priorityLabel.setAttribute('for', 'project-priority');
+      priorityLabel.textContent = 'Priority';
+      // CREATE BOX TO HOLD RADIO BUTTONS AND LABELS
+      const projectPriorityBox = document.createElement('div');
+      if (a === true) {
+        projectPriorityBox.value = d;
+      }
+
+      // CREATE LOW PRIORITY RADIO BUTTON
+      const lowLabel = document.createElement('label');
+      lowLabel.setAttribute('for', 'low-priority');
+      lowLabel.textContent = 'low';
+      const lowPriority = document.createElement('input');
+      lowPriority.id = 'low-priority';
+      lowPriority.setAttribute('type', 'radio');
+      lowPriority.setAttribute('name', 'project-priority');
+      lowPriority.setAttribute('value', 'low');
+      // CHECK LOW IF EDITING AND VALUE OF CURRENT OBJ IS LOW
+      if (projectPriorityBox.value === 'low') {
+        lowPriority.checked = true;
+      }
+      projectPriorityBox.appendChild(lowLabel);
+      projectPriorityBox.appendChild(lowPriority);
+
+      // CREATE MEDIUM PRIORITY RADIO BUTTON
+      const mediumLabel = document.createElement('label');
+      mediumLabel.setAttribute('for', 'medium-priority');
+      mediumLabel.textContent = 'medium';
+      const mediumPriority = document.createElement('input');
+      mediumPriority.id = 'medium-priority';
+      mediumPriority.setAttribute('type', 'radio');
+      mediumPriority.setAttribute('name', 'project-priority');
+      mediumPriority.setAttribute('value', 'medium');
+      // CHECK MEDIUM IF EDITING AND VALUE OF CURRENT OBJ IS MEDIUM
+      if (projectPriorityBox.value === 'medium') {
+        mediumPriority.checked = true;
+      }
+      projectPriorityBox.appendChild(mediumLabel, mediumPriority);
+      projectPriorityBox.appendChild(mediumPriority);
+
+      // CREATE HIGH PRIORITY RADIO BUTTON
+      const highLabel = document.createElement('label');
+      highLabel.setAttribute('for', 'high-priority');
+      highLabel.textContent = 'high';
+      const highPriority = document.createElement('input');
+      highPriority.id = 'high-priority';
+      highPriority.setAttribute('type', 'radio');
+      highPriority.setAttribute('name', 'project-priority');
+      highPriority.setAttribute('value', 'high');
+      // CHECK HIGH IF EDITING AND VALUE OF CURRENT OBJ IS HIGH
+      if (projectPriorityBox.value === 'high') {
+        highPriority.checked = true;
+      }
+      projectPriorityBox.appendChild(highLabel, highPriority);
+      projectPriorityBox.appendChild(highPriority);
+
+      // CREATE SUBMIT BUTTON, SELECT LABEL BASED ON EDITING STATE
+      const submitLabel = document.createElement('label');
+      submitLabel.setAttribute('for', 'project-submit');
+      const projectSubmit = document.createElement('button');
+      if (a === true) {
+        projectSubmit.textContent = 'Update project';
+      } else {
+        projectSubmit.textContent = 'Add project';
+      }
+      projectSubmit.setAttribute('id', 'form-submit');
+      projectSubmit.setAttribute('type', 'submit');
+      projectSubmit.setAttribute('name', 'project-submit');
+
+      // ADD project INPUTS TO FORM AND FORM TO PAGE
+      projectForm.append(titleLabel, projectTitle);
+      projectForm.append(dueLabel, projectDue);
+      projectForm.append(priorityLabel, projectPriorityBox);
+      projectForm.append(submitLabel, projectSubmit);
+      projectCard.appendChild(projectForm);
+      formOverlay.appendChild(projectCard);
+
+      // SUBMIT FUNCTIONALITY
+      projectForm.addEventListener('submit', (e) => {
+        // STOPS SUBMIT FROM SENDING DATA TO SEVER BY DEFAULT
+        e.preventDefault();
+        // PULL ALL RADIO INPUTS BY NAME INTO submitPriority[]
+        const submitPriotiry = document.getElementsByName('project-priority');
+        // LOOP THROUGH PRIORITY BUTTONS
+        for (let i = 0; i < submitPriotiry.length; i += 1) {
+          // IF ONE IS CHECKED, projectPriorityBox IS ASSIGNED THAT VALUE
+          if (submitPriotiry[i].checked === true) {
+            const priority = submitPriotiry[i].value;
+            projectPriorityBox.value = priority;
+          }
+        }
+
+        // IF EDITING AN ESTABLISHED project, UPDATE THAT OBJ'S (g's) VALUE
+        if (a === true) {
+          const currentPosition = f;
+          projectList[currentPosition].project = projectTitle.value;
+          projectList[currentPosition].due = projectDue.value;
+          projectList[currentPosition].priority = projectPriorityBox.value;
+        // ELSE, CREAT A NEW OBJECT WITH THESE VALUES AND PUSH IT TO projectList[]
+        } else {
+          const projectObj = newProject(
+            projectTitle.value,
+            projectDue.value,
+            projectPriorityBox.value,
+          );
+          projectList.push(projectObj);
+        }
+        // ASSIGNS THE HIDDEN ID TO HIDE THE OVERLAY
+        formOverlay.setAttribute('id', 'form-overlay');
+        // REMOVES THE FORM CARD FROM THE OVERLAY
+        formOverlay.removeChild(projectCard);
+        // REMOVES BLUE FROM BACKGROUND
+        removeBlur(pageBox);
+        // PUTS project OBJECT DATA INTO DOM TABLE
+        displayprojects();
+        console.log(projectList);
+      });
+      return projectCard;
+    };
+
+    // DISPLAY FORM, USING ARGS PASSED FROM OBJ IF FORM LAUNCHED FORM EDIT BUTTON
+    const displayForm = (a, b, c, d, f, g) => {
+      // BLUR BACKGORUND
+      // if (addNew.hasChildNodes()) {
+      //   const selectBox = document.getElementById('select-box');
+      //   selectBox.remove();
+      // }
+
+      pageBox.classList.add('blurred');
+
+      // SET formOverlay ID AND CREATE taskCard AND taskFrom
+      formOverlay.id = 'form-overlay-vis';
+      const taskCard = document.createElement('div');
+      taskCard.id = 'form-card';
+      const taskForm = document.createElement('form');
+      taskForm.id = 'form-form';
+      taskForm.setAttribute('action', '');
+      taskForm.setAttribute('method', 'post');
+      // ------------------------------------------------------------
+      // CREATE TITLE INPUT AND LABEL, SET ATTRIBUTES
+      // IF FORM LAUNCHED FORM editBtn, POPULATE W/ ARG VALUE FORM OBJ
+
       const titleLabel = document.createElement('label');
       titleLabel.setAttribute('for', 'task-title');
       titleLabel.textContent = 'Title';
@@ -186,7 +451,7 @@ const load = () => {
         taskTitle.value = b;
       }
       taskTitle.setAttribute('type', 'text');
-      taskTitle.setAttribute('id', 'task-title');
+      taskTitle.setAttribute('id', 'form-title');
       taskTitle.setAttribute('placeholder', 'Task title . . .');
       taskTitle.setAttribute('name', 'task-title');
 
@@ -199,7 +464,7 @@ const load = () => {
       if (a === true) {
         taskDescription.value = c;
       }
-      taskDescription.setAttribute('id', 'task-description');
+      taskDescription.setAttribute('id', 'form-description');
       taskDescription.setAttribute('type', 'text');
       taskDescription.setAttribute('placeholder', 'Task description . . .');
       taskDescription.setAttribute('name', 'task-description');
@@ -213,7 +478,7 @@ const load = () => {
       if (a === true) {
         taskDue.value = d;
       }
-      taskDue.setAttribute('id', 'task-due');
+      taskDue.setAttribute('id', 'form-due');
       taskDue.setAttribute('type', 'date');
       taskDue.setAttribute('placeholder', 'Task due . . .');
       taskDue.setAttribute('name', 'task-due');
@@ -297,14 +562,14 @@ const load = () => {
       taskForm.append(priorityLabel, taskPriorityBox);
       taskForm.append(submitLabel, taskSubmit);
       taskCard.appendChild(taskForm);
-      taskOverlay.appendChild(taskCard);
+      formOverlay.appendChild(taskCard);
 
-      // SUBMIT FUNTIONALITY
+      // SUBMIT FUNCTIONALITY
       taskForm.addEventListener('submit', (e) => {
         // STOPS SUBMIT FROM SENDING DATA TO SEVER BY DEFAULT
         e.preventDefault();
         // PULL ALL RADIO INPUTS BY NAME INTO submitPriority[]
-        const submitPriotiry = document.getElementsByName("task-priority");
+        const submitPriotiry = document.getElementsByName('task-priority');
         // LOOP THROUGH PRIORITY BUTTONS
         for (let i = 0; i < submitPriotiry.length; i += 1) {
           // IF ONE IS CHECKED, taskPriorityBox IS ASSIGNED THAT VALUE
@@ -327,42 +592,65 @@ const load = () => {
             taskTitle.value,
             taskDescription.value,
             taskDue.value,
-            taskPriorityBox.value
+            taskPriorityBox.value,
           );
           taskList.push(taskObj);
         }
         // ASSIGNS THE HIDDEN ID TO HIDE THE OVERLAY
-        taskOverlay.setAttribute("id", "task-overlay");
+        formOverlay.setAttribute('id', 'form-overlay');
         // REMOVES THE FORM CARD FROM THE OVERLAY
-        taskOverlay.removeChild(taskCard);
+        formOverlay.removeChild(taskCard);
         // REMOVES BLUE FROM BACKGROUND
         removeBlur(pageBox);
         // PUTS TASK OBJECT DATA INTO DOM TABLE
-        displayTasks();
+        displayTasks('task');
         console.log(taskList);
       });
       return taskCard;
     };
-
+    let selecting = null;
     // CREATE NEW TASK FORM HANDLER
-    addTask.addEventListener('click', () => {
-      displayForm();
+    addNew.addEventListener('click', () => {
+      if (selecting === true) { return; }
+      selecting = true;
+
+      const selectBox = document.createElement('div');
+      selectBox.id = 'select-box';
+      const projectSelect = document.createElement('button');
+      projectSelect.textContent = 'New Project';
+      projectSelect.id = 'project-select';
+      const taskSelect = document.createElement('button');
+      taskSelect.textContent = 'New Task';
+      projectSelect.id = 'task-select';
+      selectBox.appendChild(projectSelect);
+      selectBox.appendChild(taskSelect);
+      addBox.appendChild(selectBox);
+      taskSelect.addEventListener('click', () => {
+        selectBox.remove();
+        selecting = false;
+        displayForm();
+      });
+      projectSelect.addEventListener('click', () => {
+        selecting = false;
+        selectBox.remove();
+        displayProjectForm();
+      });
     });
 
     const clickout = (() => {
       // PUTS A CLICK LISTENER ON OVERLAY BACKGROUND FOR CLICKOUT
-      taskOverlay.addEventListener('mousedown', (e) => {
+      formOverlay.addEventListener('mousedown', (e) => {
         const card = document.getElementById('task-card');
         // MAKE clickSpot = tTHE TARGET EVENT
         const clickSpot = e.target;
-        // IF CLICK HAPPENED ON taskOverlay, I.E OUTSIDE THE FORM
-        if (clickSpot.id === 'task-overlay-vis') {
+        // IF CLICK HAPPENED ON formOverlay, I.E OUTSIDE THE FORM
+        if (clickSpot.id === 'form-overlay-vis') {
           // SET OVERLAY TO HIDDEN ID
-          taskOverlay.id = 'task-overlay';
+          formOverlay.id = 'form-overlay';
           // REMOVE BLUR
           removeBlur(pageBox);
           // REMOVE CARD FROM OVERLAY
-          taskOverlay.removeChild(card);
+          formOverlay.removeChild(card);
         }
       });
     })();
